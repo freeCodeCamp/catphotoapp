f50fac3dc297c6e2be3ac77b27917dccbc4a8723
Jonathan <theflametrooper@gmail.com>
2016-06-29 19:37:31 +0100

    Fixed some bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+  connect.server();
+});
 
 gulp.task('styles', function() {
   gulp.src('static/sass/**/*.scss')
@@ -7,6 +12,6 @@ gulp.task('styles', function() {
     .pipe(gulp.dest('static/css'))
 });
 
-gulp.task('default', function() {
+gulp.task('default', ['connect'], function() {
 	gulp.watch('static/sass/**/*.scss', ['styles']);
 });
--- static/js/main.js
+++ static/js/main.js
@@ -19,13 +19,20 @@ photosRef.once("value")
         $('.results > .row:last').append(showData(childSnapshot.val()));
       }
       $('.materialboxed').materialbox();
-      $(`.${childSnapshot.val().title} .like`).click(function() {
+
+      var identifier = childSnapshot.val().title.replace(/\s+/g, '');
+      $(`.${identifier} .like`).click(function() {
+        // $(`.${identifier} .likes_number`).text(Number($(this).val()) + 1));
+        firebase.database().ref('photos/' + childSnapshot.val().title).on('child_changed', function(snapshot) {
+          $(`.${identifier} .likes_number`).text(snapshot.val());
+        });;
         writeLikes(childSnapshot.val().title);
       });
-
-      $(`.${childSnapshot.val().title} .delete`).click(function() {
-        deletePhoto(childSnapshot.val().title);
-      });
+      
+      // $(`.${childSnapshot.val().title} .delete`).click(function() {
+      //   deletePhoto(childSnapshot.val().title);
+      //   $(`.${childSnapshot.val().title}`).remove();
+      // });
     });
   });
 
@@ -35,7 +42,7 @@ $('.submit').click(function () {
     url: $('#url').val(),
     location: $('input[name="location"]:checked').val(),
     tags: $('#tags').val(),
-    likes: 1
+    likes: 0
   });
 });
 
@@ -45,22 +52,26 @@ function writeLikes(title) {
   });
 }
 
-function deletePhoto(title) {
-  firebase.database().ref('photos/' + title).remove();
-}
+// function deletePhoto(title) {
+//   firebase.database().ref('photos/' + title).remove();
+// }
 
 function showData(photo) {
+  var identifier = photo.title.replace(/\s+/g, '');
   var html = '';
-  html += '<div class="col s3 m4 s12"><div class="card ' + photo.title + '">';
+  html += '<div class="col s3 m4 s12"><div class="card ' + identifier + '">';
     html += '<div class="card-image">';
       html += '<img class="materialboxed" src="' + photo.url + '" />';
       html += '<span class="card-title">' + photo.title + '</span>';
     html += '</div><div class="card-content">';
       html += '<p>Location: ' + photo.location + '</p>';
       html += '<p>Tags: ' + photo.tags + '</p>';
-    html += '</div><div class="card-action">';
-      html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn like"><i class="fa fa-thumbs-up"></i> ${photo.likes}</a><a class="waves-effect waves-light waves-blue blue darken-3 btn delete"><i class="fa fa-trash"></i> Delete</a>`;
-  html += '</div></div></div>';
+    html += '</div>';
+    html += `<div class="card-action">`
+      html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn like"><i class="fa fa-thumbs-up"></i> <span class="likes_number">${photo.likes}</span></a>`;
+      // html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn delete"><i class="fa fa-trash"></i> Delete</a>`;
+    html += `</div>`;
+  html += '</div></div>';
   return html;
 }
 
