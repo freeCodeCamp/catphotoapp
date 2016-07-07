ed51121fd384d7304c3a89ff2701e91b758a30b1
Jonathan <theflametrooper@gmail.com>
2016-07-07 02:10:33 +0100

    SOme more adjustments

--- src/js/main.js
+++ src/js/main.js
@@ -10,52 +10,45 @@ var photosRef = firebase.database().ref("photos").orderByKey();
 var count = 0; // Keeping track of number of images
 
 photosRef.once("value")
-  .then(function(snapshot) {    
+  .then(function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       count++;
+      var identifier = childSnapshot.val().title.replace(/\s+/g, '');
       if (count % 3 === 1) {
-        $('.results').append('<div class="row">' + showData(childSnapshot.val()) + '</div>');
+        $('.results').append('<div class="row">' + showData(childSnapshot.val(), identifier) + '</div>');
       } else {
-        $('.results > .row:last').append(showData(childSnapshot.val()));
+        $('.results > .row:last').append(showData(childSnapshot.val(), identifier));
       }
       $('.materialboxed').materialbox();
 
-      var identifier = childSnapshot.val().title.replace(/\s+/g, '');
       $(`.${identifier} .like`).click(function() {
-        // $(`.${identifier} .likes_number`).text(Number($(this).val()) + 1));
         firebase.database().ref('photos/' + childSnapshot.val().title).on('child_changed', function(snapshot) {
           $(`.${identifier} .likes_number`).text(snapshot.val());
-        });;
+        });
         writeLikes(childSnapshot.val().title);
       });
-
-      // $(`.${childSnapshot.val().title} .delete`).click(function() {
-      //   deletePhoto(childSnapshot.val().title);
-      //   $(`.${childSnapshot.val().title}`).remove();
-      // });
     });
   });
 
 $('.submit').click(function () {
-   var url = $('#url').val(),
-         tempTags = $('#tags').val(),
-         tags = tempTags.split(',');
+ var url = $('#url').val(),
+     tempTags = $('#tags').val(),
+     tags = tempTags.split(',');
 
-          if(url.match(/(jpg|png|gif)$/i) && url.match(/^http/)){
-            uploadCat(url);
-          }
-          else {
-            return alert("Please supply a URL which leads directly to a GIF, JPG or PNG file for upload.");
-          }
+  if (url.match(/(jpg|png|gif)$/i) && url.match(/^http/)) {
+    uploadCat(url);
+  } else {
+    return alert("Please supply a URL which leads directly to a GIF, JPG or PNG file for upload.");
+  }
 
-function uploadCat(url){
-      firebase.database().ref('photos/' + $('#title').val()).set({
-        title: $('#title').val(),
-        url: url,
-        location: $('input[name="location"]:checked').val(),
-        tags: tags,
-        likes: 1
-      });
+  function uploadCat(url) {
+    firebase.database().ref('photos/' + $('#title').val()).set({
+      title: $('#title').val(),
+      url: url,
+      location: $('input[name="location"]:checked').val(),
+      tags: tags,
+      likes: 1
+    });
   };
 });
 
@@ -65,31 +58,24 @@ function writeLikes(title) {
   });
 }
 
-// function deletePhoto(title) {
-//   firebase.database().ref('photos/' + title).remove();
-// }
-
-function showData(photo) {
-  var identifier = photo.title.replace(/\s+/g, '');
+function showData(photo, identifier) {
   var html = '';
-    html += '<div class="col s3 m4 s12"><div class="card ' + identifier + '">';
-    html += '<div class="card-image">';
-    html += '<img class="materialboxed" src="' + photo.url + '" />';
-    html += '<span class="card-title">' + photo.title + '</span>';
-    html += '</div><div class="card-content">';
-    html += '<p>Location: ' + photo.location + '</p>';
-    html += '<p>Tags: ';
+      html += '<div class="col s3 m4 s12"><div class="card ' + identifier + '">';
+      html += '<div class="card-image">';
+      html += '<img class="materialboxed" src="' + photo.url + '" />';
+      html += '<span class="card-title">' + photo.title + '</span>';
+      html += '</div><div class="card-content">';
+      html += '<div class="location"><i class="fa fa-map-marker"></i><p>' + photo.location + '</p></div>'
 
-for(let t = 0; t <photo.tags.length; t++){
-    html += '<span class="chip blue darken-2">' + photo.tags[t] + '</span>';
-}
+      for (let t = 0; t < photo.tags.length; t++) {
+        html += '<span class="chip blue darken-2">' + photo.tags[t] + '</span>';
+      }
 
-    html += '</p></div>';
-    html += `<div class="card-action">`
-    html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn like"><i class="fa fa-thumbs-up"></i> <span class="likes_number">${photo.likes}</span></a>`;
-//  html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn delete"><i class="fa fa-trash"></i> Delete</a>`;
-    html += `</div>`;
-    html += '</div></div>';
+      html += '</div>';
+      html += `<div class="card-action">`
+      html += `<a class="waves-effect waves-light waves-blue blue darken-3 btn like"><i class="fa fa-thumbs-up"></i> <span class="likes_number">${photo.likes}</span></a>`;
+      html += `</div>`;
+      html += '</div></div>';
   return html;
 }
 
